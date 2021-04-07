1a104745088059e2829ace921fa5ccac2551a9ae
Reiko Asakura <asakurareiko@protonmail.ch>
2021-04-07 11:08:31 -0400

    Update for Vita Development Suite Libraries 0.4.0

--- ini-file-processor/main.cpp
+++ ini-file-processor/main.cpp
@@ -18,20 +18,13 @@ struct ScePafInitParam {
 	SceInt32 unk14;
 };
 
-struct SceSysmoduleLoadOpt {
-	SceUInt32 flags;
-	SceInt32 *pRes;
-	SceInt32 unused[2];
-};
-
 int main(void) {
 	using namespace sce::Ini;
 
 	// ScePaf is required for SceIniFileProcessor
 	auto pafInit = ScePafInitParam{0x500000, 0xEA60, 0x40000, 0, 0, 0};
-	auto loadOpt = SceSysmoduleLoadOpt();
 
-	sceSysmoduleLoadModuleInternalWithArg(SCE_SYSMODULE_INTERNAL_PAF, sizeof(pafInit), &pafInit, &loadOpt);
+	sceSysmoduleLoadModuleInternalWithArg(SCE_SYSMODULE_INTERNAL_PAF, sizeof(pafInit), &pafInit, 0, NULL);
 	sceSysmoduleLoadModuleInternal(SCE_SYSMODULE_INTERNAL_INI_FILE_PROCESSOR);
 
 	auto allocator = MemAllocator{malloc, free};
@@ -41,17 +34,19 @@ int main(void) {
 	{
 		auto iniProcessor = IniFileProcessor();
 		iniProcessor.initialize(&iniInit);
+
 		if (iniProcessor.open("app0:/default.ini", "r", 0) < 0) {
-			iniProcessor.terminateForError();
+			printf("Error: 0x%08X\n", iniProcessor.getLastIoError());
 		} else {
-			char key[0x80];
+			char key[SCE_INI_FILE_PROCESSOR_KEY_BUFFER_SIZE];
 			char val[0x100];
-			while (iniProcessor.parse(key, val, 0x100) != SCE_INI_FILE_PROCESSOR_PARSE_COMPLETED) {
+			while (iniProcessor.parse(key, val, sizeof(val)) != SCE_INI_FILE_PROCESSOR_PARSE_COMPLETED) {
 				printf("Parsed %s=%s\n", key, val);
 			}
-			iniProcessor.cleanup();
-			iniProcessor.terminate();
+			iniProcessor.close();
 		}
+
+		iniProcessor.terminate();
 	}
 
 	return 0;
